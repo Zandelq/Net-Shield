4e6d03d58b7fcc04628d49abf1b8477ecde9e20f
Zandelq <133067605+Zandelq@users.noreply.github.com>
2024-12-05 13:04:07 -0600

    Update script.js

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ document.addEventListener('mousemove', (event) => {
     if (!isMouseTrailActive) return;
 
     const trail = trailElements[trailIndex];
-    
+
     // Position the trail just below the cursor
     trail.style.left = `${event.pageX - trail.offsetWidth / 2}px`; // Center the trail under the mouse
     trail.style.top = `${event.pageY + 10}px`; // Position it below the mouse cursor by 10px (adjust as needed)
