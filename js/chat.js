803a01940f2ef67a2ecada1661d579a6db20c64d
Zandelq <133067605+Zandelq@users.noreply.github.com>
2025-05-07 07:54:41 -0500

    Update chat.js

--- js/chat.js
+++ js/chat.js
@@ -148,3 +148,34 @@ async function fetchGif(query) {
   const data = await res.json();
   return data.data[0]?.images.fixed_height.url || null;
 }
+function makeDraggable(elmnt, handle) {
+  let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+
+  handle.onmousedown = dragMouseDown;
+
+  function dragMouseDown(e) {
+    e.preventDefault();
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDrag;
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e.preventDefault();
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDrag() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+// Attach it after DOM is ready
+makeDraggable(chatBox, document.getElementById("chatHeader"));
